d54ceb29a461d0a18bd3039170f348a2edcda723
Sudhanshu Kumar Swain <itsskswain@gmail.com>
2018-02-24 03:05:00 +0530

    5th event

---
+++ README.md
@@ -0,0 +1,7 @@
+# dastakfoundation.github.io
+# http://www.dastakfoundation.in/
+
+The Dastak Foundation is a national level NGO came into existence on 11 August, 2016. It is registered under the Societies Registration Act, 1860 (All INDIA LEVEL SOCIETY).
+The website is Developed by Sudhanshu Kumar Swain, who used a Template from HTML5 UP as a Design, The website is completely based on that design.
+
+The motive of website is to provide all information like events, memberships to public, Interested people can go to the website and Login to be a Volunteer/Member of the Foundation and also can donate to help Dastak Foundation to grow.
---
+++ events/assets/js/main.js
@@ -0,0 +1,105 @@
+
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Touch?
+			if (skel.vars.touch)
+				$body.addClass('is-touch');
+
+		// Forms.
+			var $form = $('form');
+
+			// Auto-resizing textareas.
+				$form.find('textarea').each(function() {
+
+					var $this = $(this),
+						$wrapper = $('<div class="textarea-wrapper"></div>'),
+						$submits = $this.find('input[type="submit"]');
+
+					$this
+						.wrap($wrapper)
+						.attr('rows', 1)
+						.css('overflow', 'hidden')
+						.css('resize', 'none')
+						.on('keydown', function(event) {
+
+							if (event.keyCode == 13
+							&&	event.ctrlKey) {
+
+								event.preventDefault();
+								event.stopPropagation();
+
+								$(this).blur();
+
+							}
+
+						})
+						.on('blur focus', function() {
+							$this.val($.trim($this.val()));
+						})
+						.on('input blur focus --init', function() {
+
+							$wrapper
+								.css('height', $this.height());
+
+							$this
+								.css('height', 'auto')
+								.css('height', $this.prop('scrollHeight') + 'px');
+
+						})
+						.on('keyup', function(event) {
+
+							if (event.keyCode == 9)
+								$this
+									.select();
+
+						})
+						.triggerHandler('--init');
+
+					// Fix.
+						if (skel.vars.browser == 'ie'
+						||	skel.vars.mobile)
+							$this
+								.css('max-height', '10em')
+								.css('overflow-y', 'auto');
+
+				});
+
+			// Fix: Placeholder polyfill.
+				$form.placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		
+
+	});
+
+})(jQuery);
